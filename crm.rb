1ccf636bb58141bb40a859b172894e917c148153
Jonathan Chang <jonoschang7@gmail.com>
2014-04-01 17:37:50 -0400

    Finish crm-web

--- crm.rb
+++ crm.rb
@@ -23,3 +23,9 @@ end
 
 get '/contacts/:id/edit' do
 end
+
+post '/contacts' do
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+	@@rolodex.add_contact(new_contact)
+	redirect to('/contacts')
+end
