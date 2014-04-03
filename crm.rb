cfb7db4805b010805c709a2d80bfb535558d72b8
Jonathan Chang <jonoschang7@gmail.com>
2014-04-03 18:02:51 -0400

    Add image to main body

--- crm.rb
+++ crm.rb
@@ -18,19 +18,6 @@ get '/contacts/new' do
 	erb :new_contact
 end
 
-get '/contacts/search' do
-	erb :search_contact
-end
-
-post '/search' do
-	@contact = @@rolodex.find(params[:id].to_i)
-	if @contact
-		erb :show_contact
-	else
-		raise Sinatra::NotFound
-	end
-end
-
 post '/contacts' do
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note], params[:date])
 	@@rolodex.add_contact(new_contact)
